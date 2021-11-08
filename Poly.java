7482ad9255bbc8dfd10a838bc11e37d227851b3c
ColdLavaLamp <85807490+ColdLavaLamp@users.noreply.github.com>
2021-11-08 15:38:12 -0800

    Added separate classes for stuff *sigh*
    oooh look, spooky second line

---
+++ Poly.java
@@ -0,0 +1,17 @@
+public static class Poly {
+   public Line[] lines;
+   public Point[] points;
+      
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Poly other = (Poly) o;
+      return MisalignSweeper.areArraysEqualDisorderly(lines, other.lines);
+   }
+
+   public Poly(Line[] lines, Point[] points) {
+      this.lines = lines;
+      this.points = points;
+   }
+}
