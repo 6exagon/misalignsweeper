419ffd61af226cadde1fecde311bc0d2430b5826
ColdLavaLamp <85807490+ColdLavaLamp@users.noreply.github.com>
2021-11-09 14:30:30 -0800

    Update Poly for better Polygon detection

--- Poly.java
+++ Poly.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Poly {
    public Line[] lines;
    public Point[] points;
@@ -9,9 +11,19 @@ public class Poly {
       Poly other = (Poly) o;
       return MisalignSweeper.areArraysEqualDisorderly(lines, other.lines);
    }
+   
+   public Point[] getPointsFromLines(Line[] lines) {
+      ArrayList<Point> ps = new ArrayList<>();
+      for (int i = 0; i < lines.length; i++) {
+         Line l = lines[i];
+         if (!ps.contains(l.points[0])) ps.add(l.points[0]);
+         if (!ps.contains(l.points[1])) ps.add(l.points[1]);         
+      }
+      return ps.toArray(new Point[0]);
+   }
 
-   public Poly(Line[] lines, Point[] points) {
+   public Poly(Line[] lines) {
       this.lines = lines;
-      this.points = points;
+      this.points = getPointsFromLines(lines);
    }
 }
