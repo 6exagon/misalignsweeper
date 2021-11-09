a87bef17a46530a880e2ac6286d826a6fef095a3
ColdLavaLamp <85807490+ColdLavaLamp@users.noreply.github.com>
2021-11-08 16:16:36 -0800

    Added separate input class

---
+++ MisalignInput.java
@@ -0,0 +1,35 @@
+import java.awt.event.*;
+import java.util.*;
+
+public class MisalignInput implements KeyListener, MouseListener {
+   
+   @Override
+   public void keyTyped(KeyEvent e) {
+      if (e.getKeyChar() == 'r') {
+         MisalignSweeper.generateBoard(new Random());
+         MisalignSweeper.FRAME.repaint();
+      }
+   }
+
+   // Required methods for KeyListener and MouseListener
+   @Override
+   public void keyPressed(KeyEvent e) { }
+
+   @Override
+   public void keyReleased(KeyEvent e) { }
+
+   @Override
+   public void mouseClicked(MouseEvent e) { }
+
+   @Override
+   public void mousePressed(MouseEvent e) { }
+
+   @Override
+   public void mouseReleased(MouseEvent e) { }
+
+   @Override
+   public void mouseEntered(MouseEvent e) { }
+
+   @Override
+   public void mouseExited(MouseEvent e) { }
+}
