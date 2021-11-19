c1a6fe9ec3b524ae63437acf6d4a389bd3e34091
ColdLavaLamp <85807490+ColdLavaLamp@users.noreply.github.com>
2021-11-18 17:52:54 -0800

    Added slope/y-intercept functionality to Line

--- Line.java
+++ Line.java
@@ -1,10 +1,18 @@
 public class Line {
    public Point[] points;
-      
+   public double m;        // slope
+   public double b;        // y-intercept
+   
    public Line(Point p1, Point p2) {
-      this.points = new Point[] {p1, p2};
+      try {
+         this.points = new Point[] {p1, p2};
+         this.m = ((double)p2.y - p1.y) / (p2.x - p1.x);
+      } catch (ArithmeticException ame) {
+         this.m = Double.MAX_VALUE;
+      }
+      this.b = p1.y - this.m * p1.x;
    }
-      
+
    @Override
    public boolean equals(Object o) {
       if (this == o) return true;
