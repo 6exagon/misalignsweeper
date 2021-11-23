ec0b030bf155854ed50de5ebcae2b98e362ba481
6exagon <55563278+6exagon@users.noreply.github.com>
2021-11-23 15:16:39 -0800

    Add Poly click detection

--- Line.java
+++ Line.java
@@ -1,10 +1,18 @@
 public class Line {
    public Point[] points;
-      
+   public double m;        // slope
+   public double b;        // y-intercept
+   
    public Line(Point p1, Point p2) {
-      this.points = new Point[] {p1, p2};
+      try {
+         this.points = new Point[] {p1, p2};
+         this.m = ((double)p2.y - p1.y) / (p2.x - p1.x);
+      } catch (ArithmeticException ame) {
+         this.m = Double.MAX_VALUE;
+      }
+      this.b = p1.y - this.m * p1.x;
    }
-      
+   
    @Override
    public boolean equals(Object o) {
       if (this == o) return true;
@@ -21,5 +29,10 @@ public class Line {
 
    public Point getOtherPoint(Point p) {
       return points[0] == p ? points[1] : points[0];
+   }
+   
+   //Returns if the line's points are on either side of an x coordinate
+   public boolean spans(int x) {
+      return (x > this.points[0].x) ^ (x > this.points[1].x);
    }
 }
