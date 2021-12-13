12b8706ff29b10ec4025531f99f94178c6f08a16
ColdLavaLamp <85807490+ColdLavaLamp@users.noreply.github.com>
2021-12-12 19:37:44 -0800

    Removed a debug printout

--- MisalignInput.java
+++ MisalignInput.java
@@ -6,9 +6,7 @@ public class MisalignInput implements MouseListener {
    // Required methods for MouseListener
    @Override
    public void mouseClicked(MouseEvent e) {
-      long t = System.nanoTime();
       Poly poly = MisalignSweeper.getClickedPolygon((int)(e.getX() / MisalignGraphics.xMultiplier), (int)(e.getY() / MisalignGraphics.yMultiplier));
-      System.out.println(System.nanoTime() - t);
       if (poly != null) {
          MisalignSweeper.repaint();
       } else {
